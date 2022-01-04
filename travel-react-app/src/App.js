0e57ac526af70e2d2431bda8776a07d694f8c6d2
Samir <Saitwalsamir@gmail.com>
2022-01-04 19:46:24 +0530

    --Image URL in CSS file Added

--- travel-react-app/src/App.js
+++ travel-react-app/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/services' exact component={Services} />
+          <Route path='/products' exact component={Products} />
+          <Route path='/sign-up' exact component={SignUp} />
         </Switch>
       </Router>
     </>
--- travel-react-app/src/components/Cards.js
+++ travel-react-app/src/components/Cards.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import CardItem from './CardItem'
 import './Cards.css'
-function Cards () {
+function Cards() {
     return (
         <div className='cards'>
             <h1>
@@ -11,10 +11,37 @@ function Cards () {
                 <div className="cards__wrapper">
                     <ul className='cards__items'>
                         <CardItem
-                        src='images/img-9.jpg'
-                        text="Explore the hidden waterfall depp inside the Amazon Jungle"
-                        lable="Adventure"
-                        path='/services'
+                            src='images/img-9.jpg'
+                            text="Explore the hidden waterfall depp inside the Amazon Jungle"
+                            label="Adventure"
+                            path='/services'
+                        />
+                        <CardItem
+                            src='images/img-2.jpg'
+                            text="Travel through the Island of Bali in A Private Cruise"
+                            label="Luxury"
+                            path='/services'
+                        />
+                    </ul>
+
+                    <ul className='cards__items'>
+                        <CardItem
+                            src='images/img-3.jpg'
+                            text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+                            label="Mystery"
+                            path='/services'
+                        />
+                        <CardItem
+                            src='images/img-4.jpg'
+                            text="Explore the Football on Top of the Himalayan"
+                            label="Adventure"
+                            path='/products'
+                        />
+                                                <CardItem
+                            src='images/img-8.jpg'
+                            text="Ride through the Sahara Desert on a guided camel tour"
+                            label="Adrenaline"
+                            path='/sign-up'
                         />
                     </ul>
                 </div>
