4500c0733b4b8d2a0fbf03131ea7a83c3a6e0469
Samir <Saitwalsamir@gmail.com>
2022-01-04 20:32:07 +0530

    --Social Media Added

--- travel-react-app/src/components/Footer.js
+++ travel-react-app/src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from './Button'
 import './Footer.css';
+import { Link } from 'react-router-dom';
 function Footer() {
     return (
         <div className='footer-container'>
@@ -13,19 +14,109 @@ function Footer() {
                 </p>
                 <div className='input-areas'>
                     <form>
-                        <input 
-                        type="email" 
-                        name='email' 
-                        placeholder='Your Email' 
-                        className="footer-input" />
+                        <input
+                            type="email"
+                            name='email'
+                            placeholder='Your Email'
+                            className="footer-input" />
                         <Button
-                        buttonStyle="btn--outline">
+                            buttonStyle="btn--outline">
                             Subscribe
                         </Button>
                     </form>
                 </div>
             </section>
+            <div className='footer-links'>
+                <div className='footer-link-wrapper'>
+                    <div className='footer-link-items'>
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it works</Link>
+                        <Link to=''>Testimonials</Link>
+                        <Link to=''>Careers</Link>
+                        <Link to=''>Investors</Link>
+                        <Link to=''>Terms of Services</Link>
+                    </div>
 
+                    <div className='footer-link-items'>
+                        <h2>Contact Us</h2>
+                        <Link to='/'>Contact</Link>
+                        <Link to='/'>Support</Link>
+                        <Link to='/'>Destinations</Link>
+                        <Link to='/'>Sponsorships</Link>
+                    </div>
+                </div>
+
+                <div className='footer-link-wrapper'>
+                    <div className='footer-link-items'>
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it works</Link>
+                        <Link to=''>Testimonials</Link>
+                        <Link to=''>Careers</Link>
+                        <Link to=''>Investors</Link>
+                        <Link to=''>Terms of Services</Link>
+                    </div>
+
+                    <div className='footer-link-items'>
+                        <h2>Videos</h2>
+                        <Link to='/'>Submit Video</Link>
+                        <Link to='/'>Ambassadors</Link>
+                        <Link to='/'>Agency</Link>
+                        <Link to='/'>Influencer</Link>
+                    </div>
+                    <div class='footer-link-items'>
+                        <h2>Social Media</h2>
+                        <Link to='/'>Instagram</Link>
+                        <Link to='/'>Facebook</Link>
+                        <Link to='/'>Youtube</Link>
+                        <Link to='/'>Twitter</Link>
+                    </div>
+                </div>
+            </div>
+            <section className="social-media">
+                <div className="social-media-wrap">
+                    <div className="footer-logo">
+                        <Link to='/' className='social-logo'>
+                            TRVL <i className='fab fa-typo3' />
+                        </Link>
+                    </div>
+                    <small class='website-rights'>TRVL © 2020</small>
+                    <div className="social-icons">
+                        <Link
+                            class='social-icon-link instagram'
+                            to='/'
+                            target='_blank'
+                            aria-label='Instagram'
+                        >
+                            <i class='fab fa-instagram' />
+                        </Link>
+
+                        <Link
+                            class='social-icon-link youtube'
+                            to='/'
+                            target='_blank'
+                            aria-label='Youtube'
+                        >
+                            <i class='fab fa-youtube' />
+                        </Link>
+                        <Link
+                            class='social-icon-link twitter'
+                            to='/'
+                            target='_blank'
+                            aria-label='Twitter'
+                        >
+                            <i class='fab fa-twitter' />
+                        </Link>
+                        <Link
+                            class='social-icon-link twitter'
+                            to='/'
+                            target='_blank'
+                            aria-label='LinkedIn'
+                        >
+                            <i class='fab fa-linkedin' />
+                        </Link>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
